e943baa1affa488eff8de505473b62eabbb42826
jaiswalsheetal21 <123313916+jaiswalsheetal21@users.noreply.github.com>
2024-03-04 15:56:51 +0530

    reactjs

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+const reactElement = {
+        
+  type: 'a',
+  
+  props: {
+  
+  href: "https://google.com",
+  
+  target: '_blank'
+   
+
+  },
+  children: 'Click me to visit google'
+}
+
+function MyApp(){
+  return(
+    <div>
+      <h1> Custom React App</h1>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    MyApp()
+ 
 )
